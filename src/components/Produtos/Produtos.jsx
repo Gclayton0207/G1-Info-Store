2df1ce56cd2c05c44ffe9c4ecd9b7954c9d8f392
Victor Barros <victorjuda49@gmail.com>
2022-06-06 17:45:22 -0300

    Adc End points e responsividade da Home

--- src/components/Produtos/Produtos.jsx
+++ src/components/Produtos/Produtos.jsx
@@ -3,32 +3,53 @@ import styles from "../Produtos/Produtos.module.css";
 import aoe from "../../Assets/Produtos/AOE.svg";
 import axios from "axios";
 
-const ProdutoAgeOfEmpires = () => {
+const Produtos = () => {
   const [softwares, setSoftwares] = useState([]);
+  const [hardwares, setHardwares] = useState([]);
+  const [produtos, setProdutos] = useState([]);
 
+  
   useEffect(() => {
     async function Api() {
       try {
         const { data } = await axios(
-          "https://projeto-final-m4.herokuapp.com/softwares"
+          "https://projeto-final-m4.herokuapp.com/softwares" 
         );
         setSoftwares(data);
+        setProdutos([...data])
         console.log();
       } catch (err) {
         console.error("ops! ocorreu um erro" + err);
       }
     }
-    Api();
+    async function Api2() {
+      try {
+        const { data } = await axios(
+          "https://projeto-final-m4.herokuapp.com/hardwares" 
+        );
+        setHardwares(data);
+        setProdutos((produtosAtuais)=>[...produtosAtuais, ...data])
+        
+        console.log(produtos);
+      } catch (err) {
+        console.error("ops! ocorreu um erro" + err);
+      }
+    }
+    Api()
+    Api2();
+
   }, []);
+
+  
+
  return(
    <div  className={styles.ProdutosContainer}>
-   {softwares.length>0 && softwares.map((software, index)=> (
+   {produtos.length>0 && produtos.map((software, index)=> (
         <div key={index}className={styles.card}>
           <div className={styles.card2}>
             <img src={aoe} className={styles.img} />
             <div className={styles.info}>
               <p>{software.nome}</p>
-              <p>{software.marca}</p>
               <p>R${software.preço.toFixed(2).replace(".", ",")}</p>
             </div>
             <div className={styles.containerBotom}>
@@ -44,9 +65,10 @@ const ProdutoAgeOfEmpires = () => {
           </div>
    ))
         }
-    </div>
-
-)
+ </div>
+        
+    )
+    
 }
 
-export default ProdutoAgeOfEmpires;
+export default Produtos;
