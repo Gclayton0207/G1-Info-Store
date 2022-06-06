56d741294e297738c066222d793cc259f01f9933
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-06-06 18:02:34 -0300

    correcao titulo

--- src/components/Painel/Painel.jsx
+++ src/components/Painel/Painel.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import styles from "./Painel.module.css";
 
 const Painel = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+      <nav className={styles.painel}>
+      <ul>
+        <li>
+          <span>Visualizar produtos</span>
+        </li>
+        <li>
+          <span>Editar produtos</span>
+        </li>
+        <li>
+          <span>Atualizar produtos</span>
+        </li>
+        <li>
+          <span>Deletar produtos</span>
+        </li>
+      </ul>
+    </nav>
+    
+  );
+};
 
-export default Painel
+export default Painel;
